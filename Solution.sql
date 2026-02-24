c2b8a4649939030e775b39f2bf983d3c39ba5b60
dev-sereti <devsereti@gmail.com>
2026-02-24 12:32:02 +0300

    Standardize country to “USA” when it appears as US, U.S.A., United States, etc

--- Solution.sql
+++ Solution.sql
@@ -21,3 +21,5 @@ select distinct customer_segment from customers order by customer_segment;
 --Use TRIM and INITCAP to clean casing and whitespace.
 select initcap(TRIM(first_name)) as firstname, 
 initcap(TRIM(last_name)) as lastname from customers;
+
+-- Standardize country to “USA” when it appears as US, U.S.A., United States, etc
